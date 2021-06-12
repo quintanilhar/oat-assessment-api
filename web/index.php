aa613522a48f8383153331f5719ea739ff9d4f6b
Ricardo Quintanilha <ricardo.aquintanilha@gmail.com>
2021-06-12 17:04:12 +0200

    Add Slim Framework to manage web interface

--- web/index.php
+++ web/index.php
@@ -1,3 +1,15 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-echo 'Hello World!';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world with Slim!");
+    return $response;
+});
+
+$app->run();
